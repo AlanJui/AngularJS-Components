3c3b21330af30716a44bed84d5c6b0dbb7b18438
居正中 <AlanJui.1960@gmail.com>
2016-11-16 13:17:36 +0800

    (1) 加入用戶端單元測試（Karma）個案，測試 movieList Component; (2) 訂正 .jshintrc 使其程式碼檢視功能，可讀懂如：describe, it, expect.... 等，Jasmine 專用之 function

---
+++ ps-movies/movie-list.component.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('The movieList component', function () {
+
+  beforeEach(module('psMovies'));
+
+  var movieList;
+
+  beforeEach(inject(function ($componentController) {
+    movieList = $componentController('movieList', {
+      $scope: {}
+    });
+  }));
+
+  it('can be created', function () {
+    expect(movieList).toBeDefined();
+    expect(movieList.$onInit).toBeDefined();
+  });
+
+});
