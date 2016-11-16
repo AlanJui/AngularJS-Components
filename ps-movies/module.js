1d8e9fd089f13649d4b8c7b6e9ce98498519ea7b
居正中 <AlanJui.1960@gmail.com>
2016-11-16 17:46:12 +0800

    使用 ngComponentRouter 切換網頁

--- ps-movies/module.js
+++ ps-movies/module.js
@@ -2,21 +2,10 @@
   'use strict';
 
   var module = angular.module('psMovies', [
-    'ngRoute'
+    'ngComponentRouter'
   ]);
 
-  module.config(function($routeProvider) {
-    $routeProvider
-      .when('/list', {
-        template: '<movie-list></movie-list>'
-      })
-
-      .when('/about', {
-        template: '<app-about></app-about>'
-      })
-
-      .otherwise({ redirectTo: '/list' });
-  });
+  module.value('$routerRootComponent', 'movieApp');
 
   module.component('appAbout', {
     template: 'This is the about page'
---
+++ ps-movies/movie-app.component.js
@@ -0,0 +1,34 @@
+(function () {
+  /* jshint validthis: true */
+  'use strict';
+
+  function movieAppCtrl() {
+    var vm = this;
+
+
+  }
+
+  var module = angular.module('psMovies');
+
+  module.component('movieApp', {
+    templateUrl: '/ps-movies/movie-app.component.html',
+    $routeConfig: [
+      {
+        path: '/list',
+        component: 'movieList',
+        name: 'List'
+      },
+      {
+        path: '/about',
+        component: 'appAbout',
+        name: 'About'
+      },
+      {
+        path: '/**',
+        redirectTo: ['List']
+      }
+    ],
+    controllerAs: 'vm',
+    controller: movieAppCtrl
+  });
+}());
